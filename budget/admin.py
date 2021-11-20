82ae0eb0156e7b53e8065ad02e505d11e2216d0d
James Gregory <james@asdfractal.com>
2021-11-20 22:40:50 +1100

    create models for income, expense, savings, category

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Income, Expenses, Savings, Category
 
-# Register your models here.
+
+class IncomeAdmin(admin.ModelAdmin):
+    model = Income
+
+
+class ExpensesAdmin(admin.ModelAdmin):
+    model = Expenses
+
+
+class SavingsAdmin(admin.ModelAdmin):
+    model = Savings
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Expenses, ExpensesAdmin)
+admin.site.register(Savings, SavingsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- budget/models.py
+++ budget/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+CURENT_USER = get_user_model()
+
+
+class Frequency(models.IntegerChoices):
+    """
+    Choices for frequency of a transaction.
+    """
+
+    WEEKLY = 52, _("Weekly")
+    BI_WEEKLY = 26, _("Bi-weekly")
+    SEMI_MONTHLY = 24, _("Semi-monthly")
+    MONTHLY = 12, _("Monthly")
+    QUARTERLY = 4, _("Quarterly")
+    SEMIANNUALLY = 2, _("Semi-annually")
+    ANNUALLY = 1, _("Annually")
+
+
+class Income(models.Model):
+    user = models.ForeignKey(CURENT_USER, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    gross_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    gross_paycheck = models.DecimalField(max_digits=10, decimal_places=2)
+    net_paycheck = models.DecimalField(max_digits=10, decimal_places=2)
+    frequency = models.IntegerField(choices=Frequency.choices, default=Frequency.WEEKLY)
+
+    def __str__(self):
+        return self.name
+
+
+class Expenses(models.Model):
+    user = models.ForeignKey(CURENT_USER, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    category = models.OneToOneField("Category", on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    frequency = models.IntegerField(choices=Frequency.choices, default=Frequency.WEEKLY)
+    monthly_frequency = models.IntegerField(default=0)
+    per_paycheck_cost = models.IntegerField(default=0)
+    annual_cost = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def _set_monthly_frequency(self):
+        if self.frequency >= 12:
+            self.monthly_frequency = self.frequency / 12
+        else:
+            self.monthly_frequency = 12 / self.frequency
+
+    def save(self, *args, **kwargs):
+        self._set_monthly_frequency()
+        super().save(*args, **kwargs)
+
+
+class Savings(models.Model):
+    user = models.ForeignKey(CURENT_USER, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
