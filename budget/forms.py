a775eb6799b93d46df81d0c6970afcf787067d02
James Gregory <james@asdfractal.com>
2021-11-21 12:17:17 +1100

    create form and view for updating and displaying expense data

--- budget/forms.py
+++ budget/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Income
+from .models import Income, Expenses, Savings
 
 
 class IncomeForm(forms.ModelForm):
@@ -10,6 +10,20 @@ class IncomeForm(forms.ModelForm):
             "gross_salary",
             "gross_paycheck",
             "net_paycheck",
+            "frequency",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expenses
+        fields = (
+            "name",
+            "category",
+            "amount",
             "frequency",
         )
 
--- budget/views.py
+++ budget/views.py
@@ -8,13 +8,17 @@ from . import forms
 def home(request):
     page_title = "Flexbudget"
     income_form = forms.IncomeForm()
+    expense_form = forms.ExpenseForm()
 
     user_income = models.Income.objects.filter(user=request.user)
+    user_expenses = models.Expenses.objects.filter(user=request.user)
 
     context = {
         "page_title": page_title,
         "income_form": income_form,
+        "expense_form": expense_form,
         "user_income": user_income,
+        "user_expenses": user_expenses,
     }
 
     return render(request, "home.html", context)
@@ -28,3 +32,15 @@ def update_income(request):
         income.user = request.user
         income.save()
         return redirect(reverse("home"))
+
+
+@require_POST
+def update_expense(request):
+    form = forms.ExpenseForm(request.POST)
+    if form.is_valid():
+        expense = form.save(commit=False)
+        expense.user = request.user
+        expense.save()
+        return redirect(reverse("home"))
+    print(form.errors)
+    return redirect(reverse("home"))
