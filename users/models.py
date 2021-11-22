b6a05b2734c88f254e696441922afc004428bdea
James Gregory <james@asdfractal.com>
2021-11-23 08:06:56 +1100

    remove is active field from user

--- users/models.py
+++ users/models.py
@@ -32,13 +32,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         default=False,
         help_text=_("Designates whether the user can log into this admin site."),
     )
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
-        ),
-    )
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
     objects = UserManager()
